924ed482c9a18ee52dee9f5ebcc8e1ba3b1f5374
Daniel Pakosz <daniel.pakosz@infotower.pl>
2022-04-21 22:54:40 +0200

    Changed welcome screen to fix bug

--- ski_slope/lib/pages/welcome_screen.dart
+++ ski_slope/lib/pages/welcome_screen.dart
@@ -16,24 +16,39 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
-  int _currentPage = 0;
   final List<String> _welcomeImages = [
     "assets/welcome_screen/welcomeScreenPhoto1.jpg",
     "assets/welcome_screen/welcomeScreenPhoto2.jpg",
     "assets/welcome_screen/welcomeScreenPhoto3.jpg",
   ];
 
-  void _nextPage() => setState(() => _currentPage++);
+  int _currentPage = 0;
+  final PageController _controller = PageController(initialPage: 0);
 
-  void _previousPage() => setState(() => _currentPage--);
+  void _nextPage() => _controller.nextPage(duration: Durations.welcomePageImageChange, curve: Curves.ease);
+
+  void _previousPage() => _controller.previousPage(duration: Durations.welcomePageImageChange, curve: Curves.ease);
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final _descriptionTextStyle = Theme.of(context).textTheme.headline2!.copyWith(
           color: SkiColors.mainColor,
-          fontSize: 15,
+          fontSize: 16,
           fontWeight: FontWeight.w400,
         );
+    const _headlineTextStyle = TextStyle(
+      fontWeight: FontWeight.w600,
+      color: SkiColors.buttonsColor,
+      fontSize: 28,
+      letterSpacing: 2,
+    );
+    double screenHeight = MediaQuery.of(context).size.height;
 
     String headline = context.text.welcomeScreenHeadline1;
     String description = context.text.welcomeScreenDescription1;
@@ -44,112 +59,109 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
       headline = context.text.welcomeScreenHeadline3;
       description = context.text.welcomeScreenDescription3;
     }
-    double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
-          Material(
-            color: SkiColors.backgroundColor,
-            child: Stack(
-              alignment: Alignment.bottomCenter,
-              children: [
-                Row(
+          PageView(
+            onPageChanged: (page) => setState(() => _currentPage = page),
+            controller: _controller,
+            children: _welcomeImages.mapToList(
+              (e) => Image.asset(
+                e,
+                alignment: Alignment.topCenter,
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: screenHeight * 0.40,
+                maxHeight: screenHeight * 0.42,
+                minWidth: double.infinity,
+              ),
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: Dimensions.paddingBigPlus),
+                decoration: BoxDecoration(
+                  color: SkiColors.backgroundColor,
+                  borderRadius: SkiRadius.welcomeScreenBorderRadius,
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
                   children: [
+                    Text(
+                      headline,
+                      textAlign: TextAlign.center,
+                      style: _headlineTextStyle,
+                    ),
                     Expanded(
-                      child: Image.asset(
-                        _welcomeImages[_currentPage],
-                        fit: BoxFit.cover,
-                        height: screenHeight > 600 ? screenHeight * 0.65 : screenHeight * 0.60,
-                        alignment: Alignment.topCenter,
+                      flex: 90,
+                      child: Container(
+                        alignment: Alignment.center,
+                        padding: const EdgeInsets.symmetric(horizontal: Dimensions.menuItemsPadding),
+                        child: Text(
+                          description,
+                          style: _descriptionTextStyle,
+                          textAlign: TextAlign.center,
+                          maxLines: 7,
+                          overflow: TextOverflow.ellipsis,
+                        ),
                       ),
                     ),
+                    Expanded(
+                      flex: 30,
+                      child: Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Flexible(
+                              child: FractionallySizedBox(
+                                widthFactor: 0.6,
+                                child: SkiButton(
+                                  child: Text(context.text.welcomePrevious),
+                                  onPressed: _currentPage == 0 ? null : _previousPage,
+                                ),
+                              ),
+                            ),
+                            for (int i = 0; i < _welcomeImages.length; i++)
+                              AnimatedContainer(
+                                width: Dimensions.welcomePageButtonPadding / 10,
+                                height: Dimensions.welcomePageButtonPadding / 4,
+                                duration: Durations.welcomePage,
+                                margin: const EdgeInsets.all(Dimensions.paddingSmall),
+                                decoration: BoxDecoration(
+                                  color: i == _currentPage ? SkiColors.mainColor : SkiColors.buttonsColor,
+                                  shape: BoxShape.circle,
+                                ),
+                              ),
+                            Flexible(
+                              child: FractionallySizedBox(
+                                widthFactor: 0.6,
+                                child: SkiButton(
+                                  child: Text(_currentPage == _welcomeImages.length - 1
+                                      ? context.text.login
+                                      : context.text.welcomeNext),
+                                  onPressed: _currentPage == _welcomeImages.length - 1
+                                      ? () => navigateToPage(
+                                            context,
+                                            builder: (context) => const LoginScreen(),
+                                          )
+                                      : _nextPage,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
                   ],
                 ),
-                Container(
-                  height: 20,
-                  decoration: BoxDecoration(
-                    color: SkiColors.backgroundColor,
-                    borderRadius: SkiRadius.welcomeScreenBorderRadius,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            flex: 15,
-            child: Text(
-              headline,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontWeight: FontWeight.w600,
-                color: SkiColors.buttonsColor,
-                fontSize: 28,
-                letterSpacing: 2,
               ),
             ),
           ),
-          Expanded(
-            flex: 60,
-            child: Container(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: Text(
-                description,
-                style: _descriptionTextStyle,
-                textAlign: TextAlign.center,
-                maxLines: 6,
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 25,
-            child: Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Flexible(
-                    child: FractionallySizedBox(
-                      widthFactor: 0.6,
-                      child: SkiButton(
-                        child: Text(context.text.welcomePrevious),
-                        onPressed: _currentPage == 0 ? null : _previousPage,
-                      ),
-                    ),
-                  ),
-                  for (int i = 0; i < _welcomeImages.length; i++)
-                    AnimatedContainer(
-                      width: Dimensions.welcomePageButtonPadding / 10,
-                      height: Dimensions.welcomePageButtonPadding / 4,
-                      duration: Durations.welcomePage,
-                      margin: const EdgeInsets.all(Dimensions.paddingSmall),
-                      decoration: BoxDecoration(
-                        color: i == _currentPage ? SkiColors.mainColor : SkiColors.buttonsColor,
-                        shape: BoxShape.circle,
-                      ),
-                    ),
-                  Flexible(
-                    child: FractionallySizedBox(
-                      widthFactor: 0.6,
-                      child: SkiButton(
-                        child: Text(
-                            _currentPage == _welcomeImages.length - 1 ? context.text.login : context.text.welcomeNext),
-                        onPressed: _currentPage == _welcomeImages.length - 1
-                            ? () => navigateToPage(
-                                  context,
-                                  builder: (context) => const LoginScreen(),
-                                )
-                            : _nextPage,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          )
         ],
       ),
     );
--- ski_slope/lib/resources/durations.dart
+++ ski_slope/lib/resources/durations.dart
@@ -2,4 +2,5 @@ class Durations {
   Durations._();
 
   static const welcomePage = Duration(milliseconds: 250);
+  static const welcomePageImageChange = Duration(milliseconds: 500);
 }
